2c01e9ccf8a1ccf1ee329b87ce5d75bd3c88dede
restaandy <andyresta@mhs.dinus.ac.id>
2017-11-26 23:59:09 +0700

    26/11/2017

--- application/logs/log-2017-11-26.php
+++ application/logs/log-2017-11-26.php
@@ -2,3 +2,11 @@
 
 ERROR - 2017-11-26 14:06:37 --> 404 Page Not Found: Assets/vendor
 ERROR - 2017-11-26 14:07:41 --> 404 Page Not Found: Assets/vendor
+ERROR - 2017-11-26 17:57:25 --> 404 Page Not Found: Instansi/css
+ERROR - 2017-11-26 17:57:25 --> 404 Page Not Found: Instansi/js
+ERROR - 2017-11-26 17:57:25 --> 404 Page Not Found: Instansi/js
+ERROR - 2017-11-26 17:57:25 --> 404 Page Not Found: Instansi/css
+ERROR - 2017-11-26 17:57:25 --> 404 Page Not Found: Instansi/js
+ERROR - 2017-11-26 17:57:26 --> 404 Page Not Found: Instansi/js
+ERROR - 2017-11-26 17:57:26 --> 404 Page Not Found: Instansi/js
+ERROR - 2017-11-26 17:57:26 --> 404 Page Not Found: Instansi/js
